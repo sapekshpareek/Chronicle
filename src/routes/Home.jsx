6df2e7f362cc7185491d66bf18916e41127b81bd
sapekshpareek <sapekshpareek136@gmail.com>
2025-01-22 19:30:06 +0530

    Basic Setup

---
+++ src/routes/Home.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+
+const Home = () => {
+    return (
+        <Box>
+            <Box className="text-red-500">GNews</Box>
+            <Box>NewsApi</Box>
+            <Box>News</Box>
+        </Box>
+    );
+};
+
+export default Home;
